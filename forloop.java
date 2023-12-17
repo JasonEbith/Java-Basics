f365ee62b2374bb28db70855d2955e76fda1b385
Jason Ebith <107571960+JasonEbith@users.noreply.github.com>
2023-12-17 21:01:15 +0530

    initialchange

---
+++ forloop.java
@@ -0,0 +1,11 @@
+public class forloop {
+    public static void main(String argStrings[]){
+        int x;
+        for(x=0;x<10;x=x+1)
+        {
+            System.out.println("this is x:" +x);
+        }
+
+    }
+    
+}
